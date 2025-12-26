64b60e65ed5d61604307e84891ed2b8869c18805
hjortie <elinhjortskull@live.se>
2025-12-26 15:52:27 +0100

    feat: load and display incoming and outgoing trades in Dashboard component

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,14 +1,36 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { BooksContext } from "../contexts/BooksContext";
 import { BookCard } from "../components/BookCard";
 import { BookInfoForm } from "../components/BookInfoForm";
 import { useMe } from "../hooks/useMe";
+import { PopTrade, Trade } from "../models/Trade";
+import { UserTrades } from "../components/UserTrades";
+import { getTrades } from "../services/tradeServices";
 
 export const Dashboard = () => {
   const { books } = useContext(BooksContext);
-  const { user, fetched, error } = useMe();
+  const [fetched, setFetched] = useState(false);
+  const [incomingTrades, setIncomingTrades] = useState<PopTrade[]>([]);
+  const [outgoingTrades, setOutgoingTrades] = useState<PopTrade[]>([]);
+  const { user, error } = useMe();
 
   const ownedBooks = books.filter((b) => b.ownerId === user?._id);
+  useEffect(() => {
+    const load = async () => {
+      try {
+        const trades = await getTrades();
+        setIncomingTrades(trades.incoming);
+        setOutgoingTrades(trades.outgoing);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setFetched(true);
+      }
+    };
+    if (!fetched) {
+      load();
+    }
+  }, [fetched]);
 
   if (!fetched) return <p>laddar...</p>;
   if (!user)
@@ -24,12 +46,21 @@ export const Dashboard = () => {
         <div className="dashboard container">
           <h1>{`Hej ${user.username}`}</h1>
           <span>{error ? `${error}` : ``}</span>
+
+          {incomingTrades.length !== 0 && (
+            <UserTrades trades={incomingTrades} type="incoming" />
+          )}
+          {outgoingTrades.length !== 0 && (
+            <UserTrades trades={outgoingTrades} type="outgoing" />
+          )}
+
           <div className="books-container row">
             <h2>Dina böcker</h2>
             {ownedBooks.map((book) => (
               <BookCard book={book} key={book._id} />
             ))}
           </div>
+
           <h2>Lägg till fler böcker</h2>
           <BookInfoForm action="post" />
         </div>
