951ac1bfd6bf4c9346192d4ce62d3f7dcaa050c2
hjortie <elinhjortskull@live.se>
2025-12-26 15:50:07 +0100

    refactor: brooke trade-model into separate types to avoid union-confusion in jsx

--- frontend/src/models/Trade.ts
+++ frontend/src/models/Trade.ts
@@ -4,8 +4,18 @@ export type Trade = {
   _id: string;
   requesterId: string; //User-id
   receiverId: string; //user-id
-  offeredBooks: string[] | Book[]; //bok-id:n - populeras i vissa lägen i backend
-  requestedBook: string | Book; //bok-id - populeras i vissa lägen i backend
+  offeredBooks: string[]; //bok-id:n - populeras i vissa lägen i backend
+  requestedBook: string; //bok-id - populeras i vissa lägen i backend
+  acceptedOfferedBook: string | null; //i backend defaultar värdet till null och ej undefined
+  status: string;
+};
+
+export type PopTrade = {
+  _id: string;
+  requesterId: string; //User-id
+  receiverId: string; //user-id
+  offeredBooks: Book[]; //bok-id:n - populeras i vissa lägen i backend
+  requestedBook: Book; //bok-id - populeras i vissa lägen i backend
   acceptedOfferedBook: string | null; //i backend defaultar värdet till null och ej undefined
   status: string;
 };
